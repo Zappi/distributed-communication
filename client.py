f00cf495744f05ae855568cea795cc9cfedef16a
Jerry <jerry.tamlander@perfektio.fi>
2019-11-14 17:49:11 +0200

    client input handler

--- client.py
+++ client.py
@@ -1,5 +1,5 @@
 import json
-import threading
+from threading import Thread
 import socket
 import sys
 
@@ -7,17 +7,19 @@ import sys
 class Client():
 
     def __init__(self, client_port):
+        self.server_port = 8888
         self.identifier = None
-        self.server_listener = SocketThread(self, client_port)
-        self.server_listener.run()
+        self.server_listener = SocketThread(
+            self, client_port, self.server_port)
+        self.server_listener.start()
 
 
-class SocketThread():
-
-    def __init__(self, client, client_port):
+class SocketThread(Thread):
+    def __init__(self, client, client_port, server_port):
+        Thread.__init__(self)
         self.client = client
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.bind(('0.0.0.0', client_port))
+        self.sock.connect(('127.0.0.1', server_port))
 
     def run(self):
         while True:
@@ -26,7 +28,22 @@ class SocketThread():
             try:
                 data = json.loads(data)
                 print(data)
+            finally:
+                sock.close()
+                print('ok boomer')
 
 
 if __name__ == '__main__':
-    print('vittu')
+    client = Client(9999)
+    while True:
+
+        cmd = input('> ')
+
+        if cmd.startswith('play'):
+            print('pelaa :)')
+        elif cmd.startswith('msg'):
+            print('message')
+        elif cmd.startswith('pmsg'):
+            print('play and message')
+        else:
+            print('Invalid command')
